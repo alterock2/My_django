b0cb9c9077a7a6d024b29dc9d820fca9d467c814
alterock2_home <alterock2@gmail.com>
2023-03-11 23:10:41 +0300

    изменение шаблона, статики

--- store/products/views.py
+++ store/products/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store'
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': [{'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                      'name': 'Худи черного цвета с монограммами adidas Originals',
+                      'price': 6090,
+                      'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'}]
+    }
+    return render(request, 'products/products.html', context)
