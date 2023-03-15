8cecea2098c589b430c17c0955bb1b88e142fecf
alterock2 <alterock2@gmail.com>
2023-03-15 16:08:01 +0300

    добавление в бд-3

--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory
 
 # Create your views here.
 def index(request):
@@ -10,9 +11,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - Каталог',
-        'products': [{'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                      'name': 'Худи черного цвета с монограммами adidas Originals',
-                      'price': 6090,
-                      'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'}]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
     }
     return render(request, 'products/products.html', context)
