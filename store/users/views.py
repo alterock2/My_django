333c67abc8711f8ab53806e90968cda23571f03d
alterock2 <alterock2@gmail.com>
2023-03-17 13:28:14 +0300

    авторизация, начало

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from users.models import User
+from users.forms import UserLoginForm
+from django.contrib import auth
 
 # Create your views here.
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+    else:
+        form = UserLoginForm()
+        context = {'form':  form}
+    return render(request, 'users/login.html', context)
 
 def registration(request):
     return render(request, 'users/registration.html')
